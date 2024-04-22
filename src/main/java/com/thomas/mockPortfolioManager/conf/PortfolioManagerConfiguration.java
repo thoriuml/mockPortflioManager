e15a724c1876660869de9f6fa6cefc3ce40e3782
thoriumL <thorium_l@hotmail.com>
2024-04-22 19:37:05 +0800

    remove unnecessary jmx

--- src/main/java/com/thomas/mockPortfolioManager/conf/PortfolioManagerConfiguration.java
+++ src/main/java/com/thomas/mockPortfolioManager/conf/PortfolioManagerConfiguration.java
@@ -3,18 +3,14 @@ package com.thomas.mockPortfolioManager.conf;
 import com.thomas.mockPortfolioManager.Services.MarketData.MarketDataPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jmx.export.annotation.ManagedOperation;
-import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
-@ManagedResource
 public class PortfolioManagerConfiguration {
     @Autowired
     private MarketDataPublisher marketDataPublisher;
 
     @Scheduled(cron = "${portfolio.marketData.interval.cron}")
-    @ManagedOperation
     public void mockPublishMarketData() {
         marketDataPublisher.mockPublishMarketData();
     }
